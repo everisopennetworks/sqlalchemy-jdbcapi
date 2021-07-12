1cad84136eabef5e053bd628896f5d085fecb8e7
Neil Banguera <neilbangue@gmail.com>
2021-07-12 13:07:00 -0500

    - Add Ignite JDBC support

--- sqlalchemy_jdbcapi/__init__.py
+++ sqlalchemy_jdbcapi/__init__.py
@@ -8,3 +8,6 @@ registry.register(
 registry.register(
     "jdbcapi.oraclejdbc", "sqlalchemy_jdbcapi.oraclejdbc", "OracleJDBCDialect"
 )
+registry.register(
+    "jdbcapi.ignitejdbc", "sqlalchemy_jdbcapi.ignitejdbc", "IgniteJDBCDialect"
+)
---
+++ sqlalchemy_jdbcapi/ignitejdbc.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+import re
+from sqlalchemy.dialects.sqlite.base import SQLiteDialect
+from sqlalchemy.sql import sqltypes
+from sqlalchemy import util, exc
+from .base import MixedBinary, BaseDialect
+
+colspecs = util.update_copy(
+    SQLiteDialect.colspecs, {sqltypes.LargeBinary: MixedBinary,},
+)
+
+
+class IgniteJDBCDialect(BaseDialect, SQLiteDialect):
+    jdbc_db_name = "ignite"
+    jdbc_driver_name = "org.apache.ignite.IgniteJdbcThinDriver"
+    colspecs = colspecs
+
+    def initialize(self, connection):
+        super(IgniteJDBCDialect, self).initialize(connection)
+
+    def _driver_kwargs(self):
+        return {}
+
+    def create_connect_args(self, url):
+        if url is None:
+            return
+        # dialects expect jdbc url e.g.
+        # "jdbc:oracle:thin@example.com:1521/db"
+        # if sqlalchemy create_engine() url is passed e.g.
+        # "oracle://scott:tiger@example.com/db"
+        # it is parsed wrong
+        # restore original url
+        s: str = str(url)
+        # get jdbc url
+        jdbc_url: str = s.split("//", 1)[-1]
+        # add driver information
+        if not jdbc_url.startswith("jdbc"):
+            jdbc_url = f"jdbc:ignite:thin:@{jdbc_url}"
+        kwargs = {
+            "jclassname": self.jdbc_driver_name,
+            "url": jdbc_url,
+            # pass driver args via JVM System settings
+            "driver_args": []
+        }
+        return ((), kwargs)
+
+
+dialect = IgniteJDBCDialect
